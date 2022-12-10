7ef9dc211d4e082816f8c2cdbffe832b8b649aaf
andrearotesidev <andrearotesi@gmail.com>
2022-12-10 17:06:22 +0100

    Added styling to home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 import {WeatherService} from "../../services/weather/weather.service";
 import {LOCATION_KEY} from "../../app.const";
 import {LocationInterface} from "../../services/weather/models/location.interface";
 import {Weather} from "../../services/weather/models/weather.model";
 import {Observable} from "rxjs";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-home',
@@ -11,11 +12,19 @@ import {Observable} from "rxjs";
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  // No need to unsubscribe, everything is handled by the async pipe
   currentWeather$: Observable<Weather>;
 
-  constructor(private weatherService: WeatherService) {
+  constructor(
+    private weatherService: WeatherService,
+    private router: Router
+  ) {
     const location: LocationInterface = JSON.parse(localStorage.getItem(LOCATION_KEY));
     this.currentWeather$ = this.weatherService.getCurrentWeather(location.latitude, location.longitude);
+  }
+
+  navigateToForecast(): void {
+    this.router.navigate(['four-days']);
   }
 
 }
