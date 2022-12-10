c9b493ff50029adf829e5398af7c5b2bfb28ced2
andrearotesidev <andrearotesi@gmail.com>
2022-12-10 12:18:00 +0100

    Routing setup
    Added home & forecast routes;
    Added wildcard routes for non-matching routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./pages/home/home.component";
+import {ForecastComponent} from "./pages/forecast/forecast.component";
+import {ErrorComponent} from "./pages/error/error.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'four-days',
+    component: ForecastComponent
+  },
+  {
+    path: '**',
+    component: ErrorComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ForecastComponent } from './pages/forecast/forecast.component';
+import { ErrorComponent } from './pages/error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ForecastComponent
+    ForecastComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
