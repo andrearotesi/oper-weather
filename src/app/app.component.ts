bd678eb2b3d9699fd35361477e66fb77553b660f
andrearotesidev <andrearotesi@gmail.com>
2022-12-10 16:21:36 +0100

    LocationInterface refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {LOCATION_KEY} from "./app.const";
-import {Location} from "./services/weather/models/location.interface";
+import {LocationInterface} from "./services/weather/models/location.interface";
 
 @Component({
   selector: 'app-root',
@@ -12,7 +12,7 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     // On start, save current location on localStorage
     navigator.geolocation.getCurrentPosition(res => {
-      const location: Location = { latitude: res.coords.latitude, longitude: res.coords.longitude };
+      const location: LocationInterface = { latitude: res.coords.latitude, longitude: res.coords.longitude };
       localStorage.setItem(LOCATION_KEY, JSON.stringify(location));
     });
   }
