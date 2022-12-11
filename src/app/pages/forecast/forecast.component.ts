3cfb63d66165e8db2c31596c0eb9401b7300875d
andrearotesidev <andrearotesi@gmail.com>
2022-12-11 22:30:58 +0100

    Wired forecast page to service

--- src/app/pages/forecast/forecast.component.ts
+++ src/app/pages/forecast/forecast.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {WeatherService} from "../../services/weather/weather.service";
 import {LOCATION_KEY} from "../../app.const";
 import {LocationInterface} from "../../services/weather/models/location.interface";
+import {Weather} from "../../services/weather/models/weather.model";
 
 @Component({
   selector: 'app-forecast',
@@ -9,15 +10,26 @@ import {LocationInterface} from "../../services/weather/models/location.interfac
   styleUrls: ['./forecast.component.scss']
 })
 export class ForecastComponent implements OnInit {
+  forecast: Weather[] = [];
+  dates: string[] = [];
 
-  constructor(
-    private weatherService: WeatherService
-  ) {
-    const location: LocationInterface = JSON.parse(localStorage.getItem(LOCATION_KEY));
-    this.weatherService.getFiveDayForecast(location.latitude, location.longitude).subscribe();
-  }
+  constructor(private weatherService: WeatherService) {}
 
   ngOnInit(): void {
+    const location: LocationInterface = JSON.parse(localStorage.getItem(LOCATION_KEY));
+    this.weatherService.getNextFourDaysForecast(location.latitude, location.longitude)
+      .subscribe((res: Weather[]) => {
+        this.forecast = res;
+
+        // Get all unique dates
+        const resDates: string[] = res.map((item) => item.date);
+        this.dates = resDates.filter((item, index) => resDates.indexOf(item) === index);
+      });
+  }
+
+  forecastByDate(date: string): Weather[] {
+    // Returns the weather conditions for the corresponding date
+    return this.forecast.filter(cond => cond.date === date);
   }
 
 }
