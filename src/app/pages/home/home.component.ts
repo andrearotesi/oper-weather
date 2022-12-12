465c66e83d9868f1f74f2734602374d228b9c2f9
andrearotesidev <andrearotesi@gmail.com>
2022-12-13 00:14:16 +0100

    Added ngOnInit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {WeatherService} from "../../services/weather/weather.service";
 import {LOCATION_KEY} from "../../app.const";
 import {Weather} from "../../services/weather/models/weather.model";
@@ -11,14 +11,16 @@ import { LocationInterface } from 'src/app/services/location.interface';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   // No need to unsubscribe, everything is handled by the async pipe
   currentWeather$: Observable<Weather>;
 
   constructor(
     private weatherService: WeatherService,
     private router: Router
-  ) {
+  ) { }
+
+  ngOnInit(): void {
     const location: LocationInterface = JSON.parse(localStorage.getItem(LOCATION_KEY));
     if (location) {
       this.currentWeather$ = this.weatherService.getCurrentWeather(location.latitude, location.longitude);
