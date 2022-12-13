12248e906ea483645911c2f040b5f2012895332b
andrearotesidev <andrearotesi@gmail.com>
2022-12-13 20:13:08 +0100

    Changed format of avg temp

--- src/app/services/weather/models/weather.model.ts
+++ src/app/services/weather/models/weather.model.ts
@@ -13,7 +13,7 @@ export class Weather {
     this.conditions = data ? data.weather : [];
     // Multiply for 1000 to convert Unix to Date
     this.date = data ? new Date(data.dt * 1000) : null;
-    this.temperature = data ? Math.round(data.main.temp * 10) / 10 : null;
+    this.temperature = data ? Math.floor(data.main.temp) : null;
     this.temp_min = data ? Math.round(data.main.temp_min * 10) / 10 : null;
     this.temp_max = data ? Math.round(data.main.temp_max * 10) / 10 : null;
   }
